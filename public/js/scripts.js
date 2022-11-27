1b597dd1590f56ac82d3837b0c34703e3dc9e74d
DaGoblin <s222574652@deakin.edu.au>
2022-11-27 23:36:08 +1100

    Final commit

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,41 +1,60 @@
 const cardList = [
   {
-      title: "Kitten 2",
+      title: "Arthur",
       image: "images/Arthur.jpg",
-      link: "About Kitten 2",
-      desciption: "Demo desciption about kitten 2"
+      link: "Photo of Arthur with a toy",
+      desciption: "Is he not cute"
   },
   {
-      title: "Kitten 3",
+      title: "Gwen",
       image: "images/Gwen.jpg",
-      link: "About Kitten 3",
-      desciption: "Demo desciption about kitten 3"
+      link: "Photo of Gwen",
+      desciption: "But is Gwen cuter"
   }
 ]
 const clickMe = () => {
   alert("Thanks for clicking me. Hope you have a nice day!")
 }
 
+const submitForm = () => {
+    let formData = {};
+    formData.first_name = $('#first_name').val();
+    formData.last_name = $('#last_name').val();
+    formData.password = $('#password').val();
+    formData.email = $('#email').val();
+
+    console.log("Form Data Submitted: ", formData);
+}
+
+// Updated this I just prefer using a back tick for text and variables better for making multiple lines of clean html
 const addCards = (items) => {
   items.forEach(item => {
-      let itemToAppend = '<div class="col s4 center-align">'+
-  '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.image+'">'+
-  '</div><div class="card-content">'+
-  '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
-  '<div class="card-reveal">'+
-      '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
-      '<p class="card-text">'+item.desciption+'</p>'+
-    '</div></div></div>';
+      let itemToAppend = 
+      `<div class="col s4 center-align">
+        <div class="card medium">
+          <div class="card-image waves-effect waves-block waves-light"><img class="activator" src="${item.image}">
+          </div>
+          <div class="card-content">
+            <span class="card-title activator grey-text text-darken-4">${item.title}<i class="material-icons right">more_vert</i></span>
+            <p><a href="#">${item.link}</a></p>
+          </div>
+          <div class="card-reveal">
+            <span class="card-title grey-text text-darken-4">${item.title}<i class="material-icons right">close</i></span>
+            <p class="card-text">${item.desciption}</p>
+          </div>
+        </div>
+      </div>`;
     $("#card-section").append(itemToAppend)
   });
 }
 
-
-
 $(document).ready(function(){
-  $('.materialboxed').materialbox();
-  $('#clickMeButton').click(()=>{
-      clickMe();
-  })
-  addCards(cardList);
-});
+    $('.materialboxed').materialbox();
+    $('#formSubmit').click(()=>{
+        submitForm();
+    })
+    addCards(cardList);
+    $('.modal').modal();
+  });
+
+document.querySelector('.myclass')
